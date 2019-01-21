26add6c1f002eda5a93ed0a60b46d86fe07eaf2a
Matt Keyworth <45359792+LBHMKeyworth@users.noreply.github.com>
2019-01-21 10:51:12 +0000

    Remove comments

--- HackneyAddressesAPI/UseCases/V1/Search/Models/SearchAddressRequestValidator.cs
+++ HackneyAddressesAPI/UseCases/V1/Search/Models/SearchAddressRequestValidator.cs
@@ -10,17 +10,6 @@ namespace LBHAddressesAPI.UseCases.V1.Search.Models
             RuleFor(x => x.addressID).NotNull().NotEmpty().WithMessage("addressID must be provided");
             RuleFor(x => x.addressID).Length(14).WithMessage("addressID must be 14 characters");
 
-            //if (request.addressID == null || string.IsNullOrEmpty(request.addressID))
-            //{
-            //    throw new Exception("lpi_key must be provided");
-            //}
-            //else if (request.addressID.Length != 14)
-            //{
-            //    throw new Exception("lpi_key must be 14 characters");
-            //}
-
-
-            //RuleFor(x => x.addressID).NotNull().NotEmpty().When(m => string.IsNullOrEmpty(m.TenancyRef) && string.IsNullOrEmpty(m.LastName) && string.IsNullOrEmpty(m.Address) && string.IsNullOrEmpty(m.PostCode)).WithMessage("Please enter a search term into FirstName");
         }
     }
 }
