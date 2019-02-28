7a4d6734917e0a3927a51e6606c7776200e71e36
Matt Keyworth <45359792+LBHMKeyworth@users.noreply.github.com>
2019-02-28 09:16:29 +0000

    Removed deprecated method in controller extension

--- HackneyAddressesAPI/Controllers/V1/BaseController.cs
+++ HackneyAddressesAPI/Controllers/V1/BaseController.cs
@@ -6,11 +6,6 @@ namespace LBHAddressesAPI.Controllers.V1
 {
     public class BaseController : Controller
     {
-        public IActionResult HandleResponse<T>( IExecuteWrapper<T> result) where T: class
-        {
-            return this.ExecuteStandardResponse(result);
-        }
-
         public IActionResult HandleResponse<T>(T result) where T : class
         {
             return this.StandardResponse(result);
--- HackneyAddressesAPI/Extensions/Controller/ControllerExtensions.cs
+++ HackneyAddressesAPI/Extensions/Controller/ControllerExtensions.cs
@@ -11,21 +11,6 @@ namespace LBHAddressesAPI.Extensions.Controller
     public static class ControllerExtensions
     {
         /// <summary>
-        /// Formats ExecuteWrapper which can be positve or negative Responses into standardised APIResponses
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="controller"></param>
-        /// <param name="result"></param>
-        /// <returns></returns>
-        [Obsolete("ExecuteStandardResponse is deprecated please do not use the ExecuteWrapper Pattern")]
-        public static IActionResult ExecuteStandardResponse<T>(this Microsoft.AspNetCore.Mvc.Controller controller, IExecuteWrapper<T> result) where T  : class
-        {
-            var apiResponse = new APIResponse<T>(result);
-            //Use Extension Method to set a statusCode as well as an object
-            return controller.StatusCode(apiResponse.StatusCode, apiResponse);
-        }
-
-        /// <summary>
         /// Formats postive responses from UseCases into APIResponses
         /// </summary>
         /// <typeparam name="T"></typeparam>
