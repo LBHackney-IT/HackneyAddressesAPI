d8c7f96ccc13186a86c54a037b172653b9784df1
Matt Keyworth <45359792+LBHMKeyworth@users.noreply.github.com>
2019-03-20 10:06:18 +0000

    Added cross refs model

---
+++ HackneyAddressesAPI/Models/AddressXref.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LBHAddressesAPI.Models
+{
+    public class AddressCrossReference
+    {
+        public string crossRefKey { get; set; }
+        public Int64 UPRN { get; set; }
+        public string name { get; set; }
+        public string code { get; set; }
+        public string value { get; set; }
+        public DateTime endDate { get; set; }
+    }
+}
