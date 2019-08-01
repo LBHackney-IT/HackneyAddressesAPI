b81394b4a6ad7a2460c19d4ce0e76c97e5288fa9
Liudvikas Taluntis <liudvikast@gmail.com>
2019-08-01 14:59:13 +0100

    APIF-263 #comment Added a validation class for Search Adress endpoint input parameters

---
+++ LBHAddressesAPITest/Validation/SearchAddressValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using Moq;
+using LBHAddressesAPI.UseCases.V1.Search.Models;
+using LBHAddressesAPI.Validation;
+
+namespace LBHAddressesAPITest.Validation
+{
+    [TestFixture]
+    public class SearchAddressValidatorTests
+    {
+        private ISearchAddressValidator _classUnderTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _classUnderTest = new SearchAddressValidator();
+        }
+    }
+}
