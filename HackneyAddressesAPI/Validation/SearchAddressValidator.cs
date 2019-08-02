fcc2d1a8357e7cea6e4fd2545b4c5567024f8239
Liudvikas Taluntis <liudvikast@gmail.com>
2019-08-02 10:32:44 +0100

    APIF-234 #comment Added tests for AddressStatus validation

--- HackneyAddressesAPI/Validation/SearchAddressValidator.cs
+++ HackneyAddressesAPI/Validation/SearchAddressValidator.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation;
+using LBHAddressesAPI.UseCases.V1.Search.Models;
 
 namespace LBHAddressesAPI.Validation
 {
-    public class SearchAddressValidator : ISearchAddressValidator
+    public class SearchAddressValidator : AbstractValidator<SearchAddressRequest>, ISearchAddressValidator
     {
+        public SearchAddressValidator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- LBHAddressesAPITest/Validation/SearchAddressValidatorTests.cs
+++ LBHAddressesAPITest/Validation/SearchAddressValidatorTests.cs
@@ -6,18 +6,47 @@ using NUnit.Framework;
 using Moq;
 using LBHAddressesAPI.UseCases.V1.Search.Models;
 using LBHAddressesAPI.Validation;
+using FluentValidation;
+using FluentValidation.TestHelper;
 
 namespace LBHAddressesAPITest.Validation
 {
     [TestFixture]
     public class SearchAddressValidatorTests
     {
-        private ISearchAddressValidator _classUnderTest;
+        private SearchAddressValidator _classUnderTest;
 
         [SetUp]
         public void SetUp()
         {
             _classUnderTest = new SearchAddressValidator();
+        }
+
+        [TestCase("cat")]
+        [TestCase("provizional")]
+        [TestCase("alternative,hystorical")]
+        public void GivenAnAddressStatusValueThatDoesntMachAllowedOnes_WhenCallingValidation_ItReturnsAnError(string addressStatusVal)
+        {
+            var request = new SearchAddressRequest() { AddressStatus = addressStatusVal };
+            _classUnderTest.ShouldHaveValidationErrorFor(x => x.AddressStatus, request).WithErrorMessage("Value for the parameter is not valid.");
+        }
+
+        [TestCase("alternative")]
+        [TestCase("historical")]
+        [TestCase("approved preffered,historical")]
+        public void GivenAnAllowedAddressStatusValue_WhenCallingValidation_ItReturnsNoErrors(string addressStatusVal)
+        {
+            var request = new SearchAddressRequest() { AddressStatus = addressStatusVal };
+            _classUnderTest.ShouldNotHaveValidationErrorFor(x => x.AddressStatus, request);
+        }
+
+        [TestCase(" ")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void GivenAWhitespaceOrEmptyAddressStatusValue_WhenCallingValidation_ItReturnsAnError(string addressStatusVal)
+        {
+            var request = new SearchAddressRequest() { AddressStatus = addressStatusVal };
+            _classUnderTest.ShouldHaveValidationErrorFor(x => x.AddressStatus, request).WithErrorMessage("Address Status should not be null or empty.");
         }
     }
 }
