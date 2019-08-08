24c415113cbb2e540cec827a31137b696db51df7
Liudvikas Taluntis <liudvikast@gmail.com>
2019-08-08 18:51:04 +0100

    APIF-257 #comment Adjusted the Regex (with TDD) to also accept Outcode + Sector, as well as Outcode + Sector + First letter of Unit, as per new requirements. Updated the explonations of postcode parts. Added forgotten extra test cases in the outcode validation to cover all 6 possible Outcode cases.

--- LBHAddressesAPITest/Validation/SearchAddressValidatorTests.cs
+++ LBHAddressesAPITest/Validation/SearchAddressValidatorTests.cs
@@ -54,10 +54,12 @@ namespace LBHAddressesAPITest.Validation
         }
         #endregion
         // Below explanations all use the postcodes IG11 7QD and E5 3XW 
-        //"Incode" refers to the whole second part of the postcode (i.e. 3XW, 7QD)
-        //"Outcode" refers to the whole first part of the postcode (Letter(s) and number(s) - i.e. IG11, E5)
-        //"Area" refers to the first letter(s) of the postcode (i.e.  IG, E)
-        //"District" refers to first number(s) to appear in the postcode (i.e. 11, 5)
+        //"Incode" refers to the whole second part of the postcode (i.e. 3XW, 7QD) from (E11 3XW, W3 7QD)
+        //"Outcode" refers to the whole first part of the postcode (Letter(s) and number(s) - i.e. IG11, E5) from (IG11 9LL, E5 2LL)
+        //"Area" refers to the first letter(s) of the postcode (i.e.  IG, E) from (IG11 9LL, E5 2LL)
+        //"District" refers to first number(s) to appear in the postcode (i.e. 11, 5) from (IG11 9LL, E5 2LL)
+        //"Sector" refers to the number in the second part of the postcode (i.e. 9, 7) from (SW2 9DN, NE4 7JU)
+        //"Unit" refers to the letters in the second part of the postcode (i.e. DN, JU) from (SW2 9DN, NE4 7JU)
         #region Postcode validation
         [TestCase("CR1 3ED")]
         [TestCase("NE7")]
@@ -78,6 +80,14 @@ namespace LBHAddressesAPITest.Validation
         [TestCase("w2 5JQ")]
         [TestCase("E11 5ra")]
         public void GivenAPostCodeValueInLowerCaseAndUpperCase_WhenCallingValidation_ItReturnsNoErrors(string postCode)
+        {
+            var request = new SearchAddressRequest() { PostCode = postCode };
+            _classUnderTest.ShouldNotHaveValidationErrorFor(x => x.PostCode, request);
+        }
+
+        [TestCase("w2 ")]
+        [TestCase("E11 ")]
+        public void GivenAnOutcodeWithSpace_WhenCallingValidation_ItReturnsNoErrors(string postCode)
         {
             var request = new SearchAddressRequest() { PostCode = postCode };
             _classUnderTest.ShouldNotHaveValidationErrorFor(x => x.PostCode, request);
@@ -123,8 +133,18 @@ namespace LBHAddressesAPITest.Validation
             _classUnderTest.ShouldHaveValidationErrorFor(x => x.PostCode, request).WithErrorMessage("Must provide at least the first part of the postcode.");
         }
 
-        [TestCase("NW9")]
-        [TestCase("RH5 ")]
+        [TestCase("E9")] //A9
+        [TestCase("S5")]
+        [TestCase("S11")] //A99
+        [TestCase("W12")]
+        [TestCase("NW9")] //AA9
+        [TestCase("RH5")]
+        [TestCase("SW17")] // AA99
+        [TestCase("NE17")]
+        [TestCase("W4R")] // A9A
+        [TestCase("N1C")]
+        [TestCase("NW1W")] // AA9A
+        [TestCase("CR1H")]
         public void GivenOnlyAnOutcodePartOfPostCode_WhenCallingValidation_ItReturnsNoErrors(string postCode)
         {
             var request = new SearchAddressRequest() { PostCode = postCode };
@@ -150,6 +170,38 @@ namespace LBHAddressesAPITest.Validation
         [TestCase("EEE")]
         [TestCase("THE")]
         public void GivenThreeCharacters_WhenCallingValidation_ItReturnsAnError(string postCode)
+        {
+            var request = new SearchAddressRequest() { PostCode = postCode };
+            _classUnderTest.ShouldHaveValidationErrorFor(x => x.PostCode, request).WithErrorMessage("Must provide at least the first part of the postcode.");
+        }
+
+        [TestCase("SW11 9")]
+        [TestCase("e14 2")]
+        public void GivenAnOutcodeAndASector_WhenCallingValidation_ItReturnsNoErrors(string postCode)
+        {
+            var request = new SearchAddressRequest() { PostCode = postCode };
+            _classUnderTest.ShouldNotHaveValidationErrorFor(x => x.PostCode, request);
+        }
+
+        [TestCase("SW7 9A")]
+        [TestCase("n12 8F")]
+        public void GivenAnOutcodeAndASectorAndTheFirstLetterOfTheUnit_WhenCallingValidation_ItReturnsNoErrors(string postCode)
+        {
+            var request = new SearchAddressRequest() { PostCode = postCode };
+            _classUnderTest.ShouldNotHaveValidationErrorFor(x => x.PostCode, request);
+        }
+
+        [TestCase("N8 LL")]
+        [TestCase("NW11 AE")]
+        public void GivenAnOutcodeAndAUnit_WhenCallingValidation_ItReturnsAnError(string postCode)
+        {
+            var request = new SearchAddressRequest() { PostCode = postCode };
+            _classUnderTest.ShouldHaveValidationErrorFor(x => x.PostCode, request).WithErrorMessage("Must provide at least the first part of the postcode.");
+        }
+
+        [TestCase("S10 H")]
+        [TestCase("W1 J")]
+        public void GivenAnOutcodeAndOnlyOneLetterOfAUnit_WhenCallingValidation_ItReturnsAnError(string postCode)
         {
             var request = new SearchAddressRequest() { PostCode = postCode };
             _classUnderTest.ShouldHaveValidationErrorFor(x => x.PostCode, request).WithErrorMessage("Must provide at least the first part of the postcode.");
